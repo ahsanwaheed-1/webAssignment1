d486248a325abfbdd1e8e1bc7b2d08a14c3e5932
ahsanwaheed-1 <ahsanwaheed100@gmail.com>
2024-10-17 20:08:32 +0500

    fixed route error

--- Calculator/src/app/app.routes.ts
+++ Calculator/src/app/app.routes.ts
@@ -3,5 +3,5 @@ import {AppComponent} from "./app.component";
 import {CalculatorSiteComponent} from "./calculator-site/calculator-site.component";
 
 export const routes: Routes = [
-  {path: '/app-calculator-site', component: CalculatorSiteComponent},
+  {path: 'app-calculator-site', component: CalculatorSiteComponent},
 ];
